ddc5bb87fa52a65f8c866a12fbb558ffa01b81c7
Anthony Thompson <34thompsonas@cua.edu>
2019-02-10 13:50:21 -0500

    Initial Commit

---
+++ src/main/java/com/tonyt1272/techjobsmvc2/TechjobsMvc2Application.java
@@ -0,0 +1,13 @@
+package com.tonyt1272.techjobsmvc2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class TechjobsMvc2Application {
+
+	public static void main(String[] args) {
+		run(TechjobsMvc2Application.class, args);
+	}
+}
---
+++ src/main/java/com/tonyt1272/techjobsmvc2/controllers/SearchController.java
@@ -0,0 +1,52 @@
+package com.tonyt1272.techjobsmvc2.controllers;
+
+import com.tonyt1272.techjobsmvc2.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+   @RequestMapping(value = "results", method = RequestMethod.GET)
+   public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm ){
+       System.out.println(searchType + " " +searchTerm);
+       ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+       //model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+       System.out.println(jobs);
+       model.addAttribute("jobs", jobs);
+       model.addAttribute("total",Integer.toString(jobs.size()));
+       model.addAttribute("columns", ListController.columnChoices);
+       return "search";
+   }
+
+
+    // TODO #1 - Create handler to process search request and display results
+
+}
+
+//@Controller
+//@RequestMapping("search/results")
+//public class ResultController {
+//
+//}
+
+
+
+
+
